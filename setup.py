3643392d6b5f6f3f4f699e34aa30e307ce32e5e0
Jan Češpivo <jan.cespivo@gmail.com>
2015-02-18 15:42:04 +0100

    setup script + cleaning

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+PACKAGE = "translation_manager"
+NAME = "django-translation-manager"
+DESCRIPTION = "Django app for managing translations from admin"
+AUTHOR = "Pavel Císař, Martin Kubát, Mikuláš Mrva, COEX CZ s.r.o (http://www.coex.cz)"
+AUTHOR_EMAIL = "cisarpavel@gmail.com"
+URL = "https://github.com/COEXCZ/django-translation-manager"
+VERSION = '0.2.0'
+LICENSE = "CC BY-NC 3.0"
 
 setup(
-    name="TranslationManager",
-    version="0.1",
-
-    author="Pavel Cisar & Martin Kubat",
-    author_email="cisarpavel@gmail.com",
-
-    # Packages
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    url=URL,
     packages=["translation_manager"],
-
-    # Include additional files into the package
     include_package_data=True,
-
-    #
-    license="license.txt",
-    description="Django app for managing translations from admin",
-
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Internationalization"
+    ],
     install_requires=[
         "polib",
         "django>=1.2"
