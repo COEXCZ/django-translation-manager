2e94ba20119165328db3c8991d55de9c577c1296
Martin Kubat <martin.kubat@coex.cz>
2015-07-23 11:43:48 +0200

    version 0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = "Django app for managing translations from admin"
 AUTHOR = "Pavel Císař, Martin Kubát, Mikuláš Mrva, Jakub Ladra - COEX CZ s.r.o (http://www.coex.cz)"
 AUTHOR_EMAIL = "pavel.cisar@coex.cz"
 URL = "https://github.com/COEXCZ/django-translation-manager"
-VERSION = '0.3.1'
+VERSION = '0.3.2'
 LICENSE = "CC BY-NC 3.0"
 
 setup(
