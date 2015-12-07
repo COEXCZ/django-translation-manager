5fd21dac1463029dcb184737cba04e73519f4e43
Visgean Skeloru <visgean@gmail.com>
2015-12-07 18:16:05 +0100

    add initial testing env

---
+++ tests/runtests.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+test_dir = os.path.dirname(__file__)
+sys.path.insert(0, test_dir)
+
+
+def run_tests():
+    import django
+    from django.test.utils import get_runner
+    from django.conf import settings
+
+    django.setup()
+    test_runner = get_runner(settings)()
+    failures = test_runner.run_tests(["tests"])
+    print failures
+    sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    run_tests()
---
+++ tests/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError:
+    # Old django fix
+    from django.conf.urls.defaults import patterns, url, include
+    admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+)
