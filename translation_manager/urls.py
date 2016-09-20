e5f9ba1a863005751bbda22b71da16c83937ab5a
Michal Kaspar <michal.kaspar@coex.cz>
2016-09-20 16:57:35 +0200

    changed urls to correct form for testing and usability

--- translation_manager/urls.py
+++ translation_manager/urls.py
@@ -1,9 +1,16 @@
 from settings import get_settings
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+urlpatterns = []
+
+if get_settings('TRANSLATIONS_PROCESSING_METHOD') == 'async_django_rq':
+    urlpatterns.append(
+        url(r'^django-rq/', include('django_rq.urls'))
+    )
 
 if get_settings('TRANSLATION_ENABLE_API_COMMUNICATION'):
     from translation_manager import views
 
-    urlpatterns = [
+    urlpatterns.append(
         url(r'^(?P<language>[\w-]+)/$', views.TranslationListView.as_view(), name='translations'),
-    ]
+    )
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from translation_manager import urls as translation_urls
 
 from translation_manager.settings import get_settings
 
@@ -9,3 +10,6 @@ urlpatterns = [
 
 if get_settings('TRANSLATIONS_PROCESSING_METHOD') == 'async_django_rq':
     urlpatterns.append(url(r'^django-rq/', include('django_rq.urls')))
+
+if get_settings('TRANSLATION_ENABLE_API_COMMUNICATION'):
+    urlpatterns.append(url(r'^translations/', include(translation_urls)))
