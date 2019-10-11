a18898f562cd6df0fa9cd54dca9597af095e8dc1
Pavel Cisar <pavel.cisar@coex.cz>
2019-10-11 11:21:30 +0200

    Updated settings MIDDLEWARE

--- settings.py
+++ settings.py
@@ -36,15 +36,15 @@ INSTALLED_APPS = (
     'rest_framework'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'urls'
 
