9a7f3bad92afe71a8b3a493b361545a6c8bdf784
Milan Vlasák <krakenus02@gmail.com>
2021-09-30 14:48:49 +0200

    default_auto_field

--- translation_manager/apps.py
+++ translation_manager/apps.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 class TranslationManagerConfig(AppConfig):
     name = 'translation_manager'
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         for path in settings.LOCALE_PATHS:
